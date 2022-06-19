e1f3fd5eac424107619c07fb7097bf2c9cc61657
Daniel S <daniel.santarriaga@gmail.com>
2022-06-18 22:20:41 -0600

    Inicio del sidebar

--- components/SidebarBlog.js
+++ components/SidebarBlog.js
@@ -3,8 +3,12 @@ import React, { Component } from 'react'
 export class SidebarBlog extends Component {
   render() {
     return (
-      <div>
-        Sidebar
+      <div className='flex justify-center'>
+        <img className='w-1/2 rounded-full' src='/me.jpg' />
+        <div className='w-1/2 mx-4 my-12'>
+          <h1 className='text-xl'> Daniel Santarriaga </h1>
+          <span className='text-sm'>Developer Web</span>
+        </div>
       </div>
     )
   }
